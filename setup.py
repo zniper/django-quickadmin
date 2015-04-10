ecca31720ced546986a59e0360c3173c51136a0e
Zniper <me.zniper@gmail.com>
2015-04-10 23:14:46 +0700

    Bump to version 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 
 setup(
     name='django-quickadmin',
-    version='0.1.0',
+    version='0.1.1',
     description='Django application automatically registers all found models into admin area',
     long_description=long_description,
     url='https://github.com/zniper/django-quickadmin',
